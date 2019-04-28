36d5f5b94eeaf8d8d594e1052a8c6ef607b74065
Christoph Stock <stockulus@icloud.com>
2019-04-28 14:36:26 +0200

    update api

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,54 @@ import * as fastify from 'fastify'
 
 import { PORT } from './env'
 
-const server = fastify({ logger: true })
+const todos = [
+  {
+    id: 1,
+    title: 'DB Ticket',
+    done: true,
+    comment: 'Berlin -> Köln'
+  },
+  {
+    id: 2,
+    title: 'DB Starten',
+    done: false,
+    comment: 'Postgres'
+  }
+]
 
-server.get('/', {}, (_, reply) => {
-  reply.send('Hallo')
-})
+function main() {
+  const server = fastify({ logger: true })
 
-server.listen(PORT, () => console.log('Server is running'))
+  server.get('/', (_, reply) => {
+    reply.send(todos)
+  })
+
+  server.post('/', (request, reply) => {
+    todos.push({ ...request.body, id: todos.length + 1 })
+    reply.send({ id: todos.length })
+  })
+
+  server.put(
+    '/:id',
+    {
+      schema: {
+        params: {
+          id: { type: 'number' }
+        }
+      }
+    },
+    (request, reply) => {
+      const id = parseInt(request.params.id, 10)
+      if (id <= 0 || id > todos.length) {
+        reply.status(404).send()
+        return
+      }
+      todos[id - 1] = { ...todos[id - 1], ...request.body }
+      reply.status(204).send()
+    }
+  )
+
+  server.listen(PORT)
+}
+
+main()
